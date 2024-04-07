6573f5eb6317dd7bd30cd06de33d84e4d04ef944
Yvnn <yannsalako28@gmail.com>
2024-04-08 00:30:57 +0200

    Ajout des models et controllers des classes Directions et Départements

---
+++ MyMBOware_v2/app/Models/Departement.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Departement extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'code_dep',
+        'nom_dep',
+        // 'code_dir',
+    ];
+}
